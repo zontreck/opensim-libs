bdefed3bba12e9187dd12e109e80b081f7fd3e79
UbitUmarov <ajlduarte@sapo.pt>
2022-07-31 10:49:38 +0100

    remove confusing and redundante old versions of ODE

--- trunk/unmanaged/OpenDynamicsEngine-r1755/ode/src/collision_libccd.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LIBCCD_COLLISION_H_
-#define _LIBCCD_COLLISION_H_
-
-int dCollideCylinderCylinder(dxGeom *o1, dxGeom *o2, int flags,
-			                 dContactGeom *contact, int skip);
-
-#endif /* _LIBCCD_COLLISION_H_ */
